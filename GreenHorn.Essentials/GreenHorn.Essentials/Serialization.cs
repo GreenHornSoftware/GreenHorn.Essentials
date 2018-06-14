42ca6de1581217ad2a64f0456b5373719fd0c510
ABaker86 <adambaker@live.com>
2018-06-13 20:46:41 -0400

    Add System.Runtime.Serialization Add Serialization (Json)

---
+++ GreenHorn.Essentials/GreenHorn.Essentials/Serialization.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Text;
+
+namespace GreenHorn.Essentials
+{
+    public class Serialization
+    {
+        public string SerializeJsonFrom<T>(T objecToSerialize)
+        {
+            using (MemoryStream memoryStream = new MemoryStream())
+            using (StreamReader streamReader = new StreamReader(memoryStream, Encoding.ASCII, true))
+            {
+                DataContractJsonSerializer dataContractJsonSerializer = new DataContractJsonSerializer(typeof(T));
+                dataContractJsonSerializer.WriteObject(memoryStream, objecToSerialize);
+                memoryStream.Position = 0;
+                return streamReader.ReadToEnd();
+            }
+        }
+
+        public static T DeserializeJsonOptimized<T>(string jsonString) where T : class, new()
+        {
+            using (MemoryStream memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(jsonString)))
+            {
+                DataContractJsonSerializer dataContractJsonSerializer = new DataContractJsonSerializer(typeof(T));
+                return dataContractJsonSerializer.ReadObject(memoryStream) as T;
+            }
+        }
+    }
+}
